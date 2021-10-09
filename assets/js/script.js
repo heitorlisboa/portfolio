bcb0694fd49def86a64855d7d1771e2fef0268c4
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-10-09 14:36:53 -0300

    Refatorei o código e adicionei funcionalidades
    Refatorei um pouco do SCSS e do JS, e adicionei uma função de copiar o meu Discord.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
 (() => {
-  const buttonDarkMode = document.querySelector('[data-btn-dark-mode]')
-  buttonDarkMode.addEventListener('click', () => {
+  function toggleDarkMode() {
     document.body.classList.toggle('dark')
+  }
+
+  function copyDiscord() {
+    const discord = 'Heitor#7447'
+    const onTooltipClickText = 'Copiado!'
+    const discordTooltip = document.querySelector('[data-discord][data-tooltip]')
+    navigator.clipboard.writeText(discord)
+      .then(
+        () => { discordTooltip.setAttribute('data-tooltip', onTooltipClickText) },
+        () => { window.alert('Falha ao copiar') }
+      )
+  }
+
+  function resetTooltip(tooltip, index) {
+    tooltip.setAttribute('data-tooltip', tooltipsTexts[index])
+  }
+
+  const buttonDarkMode = document.querySelector('[data-btn-dark-mode]')
+  buttonDarkMode.addEventListener('click', toggleDarkMode)
+
+  const discordIcon = document.querySelector('[data-discord]')
+  discordIcon.addEventListener('click', copyDiscord)
+
+  const tooltips = document.querySelectorAll('[data-tooltip]')
+  var tooltipsTexts = [];
+  tooltips.forEach((tooltip, index) => {
+    tooltipsTexts.push(tooltip.getAttribute('data-tooltip'))
+    tooltip.addEventListener('mouseout', resetTooltip.bind(null, tooltip, index))
   })
 })()
