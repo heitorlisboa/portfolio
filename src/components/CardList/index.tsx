908f362b31b225896a91467c90496489ca335eea
heitorlisboa <heitor01101000@gmail.com>
2022-02-03 21:48:28 -0300

    Componente substituído
    - Componente de cartão para item de lista foi substituído por componente de lista

---
+++ src/components/CardList/index.tsx
@@ -0,0 +1,15 @@
+import type { FC, HTMLProps } from "react";
+import styles from "./CardList.module.scss";
+
+const CardList: FC<HTMLProps<HTMLUListElement>> = ({
+  children,
+  ...otherProps
+}) => {
+  return (
+    <ul className={styles.list} {...otherProps}>
+      {children}
+    </ul>
+  );
+};
+
+export default CardList;
--- src/components/MiniCard/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import styles from "./MiniCard.module.scss";
-
-const MiniCard: FC = ({ children }) => {
-  return <li className={styles.card}>{children}</li>;
-};
-
-export default MiniCard;
