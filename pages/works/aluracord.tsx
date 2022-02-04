0781ed118b94c68aaf77fcd1a80242916319696a
heitorlisboa <heitor01101000@gmail.com>
2022-02-03 22:25:43 -0300

    Estrutura inicial das páginas dos projetos

--- pages/works/aluracord.tsx
+++ pages/works/aluracord.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
+import styles from "../../src/styles/pages/Works.module.scss";
 
 import Head from "next/head";
+import CardList from "../../src/components/CardList";
+import Footer from "../../src/components/Footer";
 
 const Aluracord: NextPage = () => {
   return (
@@ -8,6 +11,58 @@ const Aluracord: NextPage = () => {
       <Head>
         <title>Heitor Lisboa - Aluracord</title>
       </Head>
+      <main>
+        <h1>Aluracord</h1>
+        <p>
+          O Aluracord consiste em um chat em tempo real, inspirado no Discord e
+          feito durante a Imersão React 2022 da Alura, porém, sem utilização de
+          nada que foi fornecido pelos instrutores, construído completamente do
+          zero utilizando Next.js
+        </p>
+        <ul className={styles.links} aria-label="Links do projeto">
+          <li>
+            <a
+              className="text-anchor"
+              href="https://github.com/heitorlisboa/aluracord"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repositório
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-anchor"
+              href="https://aluracord-heitorlisboa.vercel.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Website
+            </a>
+          </li>
+        </ul>
+        <section>
+          <h2 id="features">Principais funcionalidades</h2>
+          <ul className={styles.features} aria-labelledby="features">
+            <li>Troca de mensagens em tempo real</li>
+            <li>Sistema usuários ativos em cada canal</li>
+            <li>Gerenciamento de mensagens (deleção, modificação e menção)</li>
+            <li>
+              Sistema de canais e servidores <strong>(EM BREVE)</strong>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2 id="technologies">Tecnologias utilizadas</h2>
+          <CardList aria-aria-labelledby="technologies">
+            <li>Next.js</li>
+            <li>Typescript</li>
+            <li>Sass</li>
+            <li>Supabase</li>
+          </CardList>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
--- pages/works/cosmo-mapeamento.tsx
+++ pages/works/cosmo-mapeamento.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
+import styles from "../../src/styles/pages/Works.module.scss";
 
 import Head from "next/head";
+import CardList from "../../src/components/CardList";
+import Footer from "../../src/components/Footer";
 
 const CosmoMapeamento: NextPage = () => {
   return (
@@ -8,6 +11,53 @@ const CosmoMapeamento: NextPage = () => {
       <Head>
         <title>Heitor Lisboa - Cosmo Mapeamento</title>
       </Head>
+      <main>
+        <h1>Cosmo Mapeamento</h1>
+        <p className={styles.projectDescription}>
+          Esse projeto consiste na reconstrução de um website previamente
+          existente &ndash; o Cosmo Mapeamento &ndash; mas com um front end
+          feito totalmente do zero e de maneira responsiva e acessível
+          utilizando React
+        </p>
+        <ul className={styles.links} aria-label="Links do projeto">
+          <li>
+            <a
+              className="text-anchor"
+              href="https://github.com/heitorlisboa/cosmo-mapeamento"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repositório
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-anchor"
+              href="https://heitorlisboa.github.io/cosmo-mapeamento/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Website
+            </a>
+          </li>
+        </ul>
+        <section>
+          <h2 id="features">Funcionalidades interessantes</h2>
+          <ul className={styles.features} aria-labelledby="features">
+            <li>Navegação lateral que acompanha o scroll</li>
+            <li>Modal de imagens com carrossel</li>
+          </ul>
+        </section>
+        <section>
+          <h2 id="technologies">Tecnologias utilizadas</h2>
+          <CardList aria-aria-labelledby="technologies">
+            <li>React</li>
+            <li>Typescript</li>
+            <li>Sass</li>
+          </CardList>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
