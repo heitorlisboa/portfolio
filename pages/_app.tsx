fd34aa494961a6072a92f1ee27701ae9ad6172b3
heitorlisboa <heitor01101000@gmail.com>
2022-02-19 00:03:27 -0300

    Ajuste na importação das fontes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,16 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="favicon.png" type="image/png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Component {...pageProps} />
     </>
