f5ce625a40f91934c85887eb33913b1eafaf65ad
heitorlisboa <heitor01101000@gmail.com>
2022-02-21 00:14:11 -0300

    Adição de conteúdo e reorganização

--- pages/works/aluracord.tsx
+++ pages/works/aluracord.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import styles from "../../src/styles/pages/Works.module.scss";
 
 import Head from "next/head";
+import Container from "../../src/components/Container";
 import CardList from "../../src/components/CardList";
 import Footer from "../../src/components/Footer";
 
@@ -11,59 +12,69 @@ const Aluracord: NextPage = () => {
       <Head>
         <title>Heitor Lisboa - Aluracord</title>
       </Head>
+
       <main>
-        <h1>
-          Aluracord &mdash; <span className="wip-warning">Página em construção</span>
-        </h1>
-        <p>
-          O Aluracord consiste em um chat em tempo real, inspirado no Discord e
-          feito durante a Imersão React 2022 da Alura, porém, sem utilização de
-          nada que foi fornecido pelos instrutores, construído completamente do
-          zero utilizando Next.js
-        </p>
-        <ul className={styles.links} aria-label="Links do projeto">
-          <li>
-            <a
-              className="text-anchor"
-              href="https://github.com/heitorlisboa/aluracord"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Repositório
-            </a>
-          </li>
-          <li>
-            <a
-              className="text-anchor"
-              href="https://aluracord-heitorlisboa.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Website
-            </a>
-          </li>
-        </ul>
-        <section>
-          <h2 id="features">Principais funcionalidades</h2>
-          <ul className={styles.features} aria-labelledby="features">
-            <li>Troca de mensagens em tempo real</li>
-            <li>Sistema usuários ativos em cada canal</li>
-            <li>Gerenciamento de mensagens (deleção, modificação e menção)</li>
+        <Container>
+          <h1>
+            Aluracord &mdash;{" "}
+            <span className="wip-warning">Página em construção</span>
+          </h1>
+          <p>
+            O Aluracord consiste em um chat em tempo real, inspirado no Discord
+            e feito durante a Imersão React 2022 da Alura, porém, sem utilização
+            de nada que foi fornecido pelos instrutores, construído
+            completamente do zero utilizando Next.js
+          </p>
+          <ul className={styles.links} aria-label="Links do projeto">
             <li>
-              Sistema de canais e servidores <strong>(EM BREVE)</strong>
+              <a
+                className="text-anchor"
+                href="https://github.com/heitorlisboa/aluracord"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repositório
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-anchor"
+                href="https://aluracord-heitorlisboa.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Website
+              </a>
             </li>
           </ul>
-        </section>
-        <section>
-          <h2 id="technologies">Tecnologias utilizadas</h2>
-          <CardList aria-labelledby="technologies">
-            <li>Next.js</li>
-            <li>Typescript</li>
-            <li>Sass</li>
-            <li>Supabase</li>
-          </CardList>
-        </section>
+          <section>
+            <h2 id="features">Principais funcionalidades</h2>
+            <ul className={styles.features} aria-labelledby="features">
+              <li>Troca de mensagens em tempo real</li>
+              <li>Sistema usuários ativos em cada canal</li>
+              <li>
+                Gerenciamento de mensagens (deleção, modificação e menção)
+              </li>
+              <li>
+                Sistema de canais e servidores <strong>(EM BREVE)</strong>
+              </li>
+              <li>
+                Sistema de autenticação <strong>(EM BREVE)</strong>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2 id="technologies">Tecnologias utilizadas</h2>
+            <CardList aria-labelledby="technologies">
+              <li>Next.js</li>
+              <li>Typescript</li>
+              <li>Sass</li>
+              <li>Supabase</li>
+            </CardList>
+          </section>
+        </Container>
       </main>
+
       <Footer />
     </>
   );
