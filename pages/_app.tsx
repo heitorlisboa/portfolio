675afdf4e719bbc18826b8d63258ce7ea9c2ea45
heitorlisboa <heitor01101000@gmail.com>
2022-02-20 23:38:41 -0300

    Adição de descrição do site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="Olá, sou Heitor Lisboa! Desenvolvedor Front End e
+          estudante de Engenharia de Software."
+        />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="favicon.png" type="image/png" />
