af2ae6961b6233288a9de76a1044f71bf1127c94
heitorlisboa <heitor01101000@gmail.com>
2022-06-15 19:03:24 -0300

    feat: add error 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+
+import styles from '@/styles/pages/Error404.module.scss';
+
 export default function Error404Page() {
-  return <div></div>;
+  return (
+    <main className={styles.container}>
+      <h1>Erro 404</h1>
+      <p>
+        Opa! Parece que a página que você está tentando acessar não foi
+        encontrada...
+      </p>
+
+      <Link href="/" passHref>
+        <a className={`${styles.homeLink} text-anchor`}>
+          Voltar à página inicial
+        </a>
+      </Link>
+    </main>
+  );
 }
