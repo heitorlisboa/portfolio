85fe8e00e3fced665a6d58b997401bb7b1091e32
heitorlisboa <heitor01101000@gmail.com>
2022-06-15 18:52:07 -0300

    feat: add new font-family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;1,400;1,500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
